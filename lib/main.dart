e8748ba90197f137a072c6a7c9a74ab356256ca6
OmarKhaled00e0 <ok4966541@gmail.com>
2025-09-10 01:48:22 +0300

    create simpleblocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:bookly/Features/home/presentation/manager/newset_books_cubit/new
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/function/setup_service_locator.dart';
+import 'package:bookly/core/utils/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,6 +19,7 @@ void main() async {
   setupServiceLocator();
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const BooklyApp());
 }
 
