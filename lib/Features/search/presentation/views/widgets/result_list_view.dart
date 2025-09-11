4e0fe31d4831c56f3e14343044d3aef97d7beb5a
OmarKhaled00e0 <ok4966541@gmail.com>
2025-09-11 23:05:15 +0300

    display data

--- lib/Features/search/presentation/views/widgets/result_list_view.dart
+++ lib/Features/search/presentation/views/widgets/result_list_view.dart
@@ -1,18 +1,19 @@
+import 'package:bookly/Features/home/domain/entites/book_entity.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class ResultListView extends StatelessWidget {
-  const ResultListView({super.key});
-
+  const ResultListView({super.key, required this.books});
+  final List<BookEntity> books;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 10,
+      itemCount: books.length,
       padding: EdgeInsets.zero,
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 10),
-          child: BookListViewItem(),
+          child: BookListViewItem(books: books[index]),
         );
       },
     );
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,7 +1,11 @@
+import 'package:bookly/Features/search/presentation/manager/search_book_cubit/search_book_cubit.dart';
 import 'package:bookly/Features/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:bookly/Features/search/presentation/views/widgets/result_list_view.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:bookly/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -14,14 +18,30 @@ class SearchViewBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomSearchTextField(),
+          CustomSearchTextField(onSubmitted: (value) {
+              context.read<SearchBookCubit>().searchBooks(value);
+            },
+          ),
           Gap(20),
           Text('Search Result', style: Styles.textStyle18),
           Gap(20),
-          Expanded(child: ResultListView()),
+          Expanded(
+            child: BlocBuilder<SearchBookCubit, SearchBookState>(
+              builder: (context, state) {
+                if (state is SearchBookSuccess) {
+                  return ResultListView(books: state.books);
+                } else if (state is SearchBookFailure) {
+                  return CustomErrorWidget(errMessage: state.errMessage);
+                } else if (state is SearchBookLoading) {
+                  return CustomLoadingIndicator();
+                } else {
+                  return const Center(child: Text('Search in book....'));
+                }
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
