dfe8dcbe8461afefc7c62e2f5176830157c3e233
OmarKhaled00e0 <ok4966541@gmail.com>
2025-09-11 23:04:09 +0300

    bloc provider in data

--- lib/Features/search/presentation/views/search_view.dart
+++ lib/Features/search/presentation/views/search_view.dart
@@ -1,5 +1,10 @@
+import 'package:bookly/Features/search/data/repos/search_repo_impl.dart';
+import 'package:bookly/Features/search/domain/use_cases/search_book_use_cases.dart';
+import 'package:bookly/Features/search/presentation/manager/search_book_cubit/search_book_cubit.dart';
 import 'package:bookly/Features/search/presentation/views/widgets/search_view_body.dart';
+import 'package:bookly/core/utils/function/setup_service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -7,7 +12,14 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: SearchViewBody()),
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) => SearchBookCubit(
+            SearchBookUseCases(getIt.get<SearchRepoImpl>())
+          ),
+          child: SearchViewBody(),
+        ),
+      ),
     );
   }
 }
